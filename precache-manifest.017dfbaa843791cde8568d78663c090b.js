f43e6b1af0541f8f1b894bf60da565f866da560a
Teemu Koivisto <tkoivisto456@gmail.com>
2019-11-22 17:21:07 +0200

    Updates

---
+++ precache-manifest.017dfbaa843791cde8568d78663c090b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f924b39f0e9999dcfc9feb826a38be",
+    "url": "/search-demo-app/index.html"
+  },
+  {
+    "revision": "d4ff54b9d5e66de3ba95",
+    "url": "/search-demo-app/static/js/2.1a24a43b.chunk.js"
+  },
+  {
+    "revision": "9090c1fff63c503b5030",
+    "url": "/search-demo-app/static/js/main.7dfc265a.chunk.js"
+  },
+  {
+    "revision": "00e7b68a90d905127fea",
+    "url": "/search-demo-app/static/js/runtime~main.7f7b626b.js"
+  }
+]);
